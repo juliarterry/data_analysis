be0587b86f7d7311a8fab324c7c3573f703195c4
Julia Terry <150367543+juliarterry@users.noreply.github.com>
2025-03-31 21:50:41 -0700

    plotting by daily totals rather than monthly

--- data_extraction.R
+++ data_extraction.R
@@ -3,6 +3,7 @@ rm(list=ls())
 library(rgbif)
 library(usethis)
 library(tidyverse)
+library(lubridate)
 
 #extract occurrences from API using 'rgbif' package
 #can quickly look occurrences up using occ_search, but limited to 100,000 records
@@ -44,5 +45,83 @@ clean_data <- raw_data %>%
   #filter out only results from iNaturalist
   filter(institutionCode == "iNaturalist") %>%
   #select only cols relevant for analysis
-  select(c(decimalLatitude, decimalLongitude, eventDate)) 
+  dplyr::select(c(decimalLatitude, decimalLongitude, eventDate, month, year, day)) %>%
+  #include only years since iNaturalist was released and only including full years
+  filter(year < 2025) %>%
+  filter(2007 < year)
+
+#count observations per year-month
+monthly_counts <- clean_data %>%
+  #count total obs for each month
+  group_by(year, month) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  #group by year and calculate total annual observations
+  group_by(year) %>%
+  mutate(total = sum(n)) %>%
+  ungroup() %>%
+  #add a new column which shows monthly observations as a proportion of annual
+  mutate(prop = n/total)
+
+#let's pop in a quick test to make sure nothing has gone skew-whiff! 
+#the sum of all unique values of annual total should be equal to the number of observations
+if(sum(unique(monthly_counts$total)) == nrow(clean_data)){
+  print("all is well :)")
+}else{
+  print("oopsie woopsie")
+}
+
+#plot the data, grouping by year
+ggplot(monthly_counts, aes(x = month, y = prop, color = factor(year))) +
+  geom_line() +  
+  geom_point() + 
+  labs(title = "Observations per Month",
+       x = "Date (Month)",
+       y = "Proportion of Annual Observations",
+       color = "Year") +
+  theme_minimal()
+
+#from a first glance, it does look like the fruiting time is shifting later as we hypothesized, but we are losing some details in grouping by month, so let's increase the granularity
+daily_counts <- clean_data %>%
+  #convert the date column into date format, removing the time and year
+  mutate(date = as.Date(eventDate)) %>%
+  mutate(date = format(date, "%m-%d")) %>%
+  dplyr::select(!c(eventDate, month, day)) %>%
+  #count total obs for each day
+  group_by(year, date) %>%
+  mutate(n = n()) %>%
+  ungroup() %>%
+  #group by year and calculate total annual observations
+  group_by(year) %>%
+  mutate(total = n()) %>%
+  ungroup() %>%
+  #add a new column which shows monthly observations as a proportion of annual
+  mutate(prop = n/total)
+
+#let's pop in a quick test to make sure nothing has gone skew-whiff! 
+#the sum of all unique values of annual total should be equal to the number of observations
+if(sum(unique(daily_counts$total)) == nrow(clean_data)){
+  print("all is well :)")
+}else{
+  print("oopsie woopsie")
+}
+
+#specify date as date so it is treated as continuous in the plot
+daily_counts$date <- as.Date(daily_counts$date, format = "%m-%d")
+  
+#plot the data, grouping by year
+ggplot(daily_counts, aes(x = date, y = prop, color = factor(year))) +
+  #geom_point() +
+  geom_smooth() +  
+  labs(title = "Annual Distribution of Observations",
+       x = "Date",
+       y = "Proportion of Annual Observations",
+       color = "Year") +
+  scale_x_date(date_labels = "%b", date_breaks = "1 month") +
+  theme_minimal()  
+
+#some of those early years are looking a bit funky - perhaps we should check how many observations they have
+
+#lets do some stats and check out these results! 
+
 
